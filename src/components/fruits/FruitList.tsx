9077a41dd1131790600b345900d5c01a1f41b60a
kokk <kokk7310@gmail.com>
2025-12-29 21:57:24 +0900

    filter

--- src/components/fruits/FruitList.tsx
+++ src/components/fruits/FruitList.tsx
@@ -1,29 +1,29 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-export default function FruitList() {
-  const items = ["사과", "바나나", "오렌지"];
-  const [fruits, setfFruits] = useState(() =>
-    items.map((item) => ({ id: uuidv4(), value: item }))
-  );
-  const handleAddItem = () => {
-    setfFruits((fruits) => [{ id: uuidv4(), value: "grape" }, ...fruits]);
-  };
 
-  const isGrapeAdded = fruits.some((fruit) => fruit.value === "grape");
+export default function App() {
+  // 초기 데이터를 설정합니다.
+  const [items, setItems] = useState([
+    { id: 1, name: "Apple", category: "Fruit" },
+    { id: 2, name: "Carrot", category: "Vegetable" },
+    { id: 3, name: "Banana", category: "Fruit" },
+    { id: 4, name: "Tomato", category: "Vegetable" },
+  ]);
+  const handleDelete = (id: number) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
   return (
     <>
-      <h1>FruitList Component</h1>
+      <h3>item list</h3>
       <ul>
-        {fruits.map((fruit) => (
-          <li key={fruit.id}>
-            {/* {fruit.value} */}
-            <input type="text" placeholder={fruit.value} />
-          </li>
-        ))}
+        {items
+          .filter((item) => item.category === "Vegetable")
+          .map((item) => (
+            <li key={item.id}>
+              {item.category}-{item.name}
+              <button onClick={() => handleDelete(item.id)}>Delete</button>
+            </li>
+          ))}
       </ul>
-      <button onClick={handleAddItem} disabled={isGrapeAdded}>
-        과일 추가
-      </button>
     </>
   );
 }

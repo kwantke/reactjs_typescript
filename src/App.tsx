d3fc4220b907880e090c286bea640101ccf540a5
kokk <kokk7310@gmail.com>
2025-12-29 14:37:08 +0900

    상태 업데이트 방법

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
 
 export default function App() {
-  const [name, setName] = useState<string>("Jack");
-  const [age, setAge] = useState<number>(20);
-  const [gender, setGender] = useState<"male" | "female">("male");
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    // setCount를 여러번 작성해도 +1씩 증가한다.
+    // (React는 배치 형태로 한꺼번에 모아서 처리하기때문)
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
 
-  const handleUpdateProfile = () => {
-    setName("Mike");
-    setAge(30);
-    setGender("female");
+    // 함수형을 사용하면 (배치에 영향받지 않고) 항상 최신값 기준으로 업데이트
+    setCount((count) => count + 1); 
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
   };
   return (
     <>
-      <p>name: {name}</p>
-      <p>age: {age}</p>
-      <p>gender: {gender}</p>
-      <button onClick={handleUpdateProfile}>Update Profile</button>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement}>increment</button>
     </>
   );
 }

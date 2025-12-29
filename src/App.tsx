301dfc58f0086edc29389efc69a7afac44f04ab4
kokk <kokk7310@gmail.com>
2025-12-29 14:56:07 +0900

    객체 상태 정의하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import { useState } from "react";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const handleIncrement = () => {
-    //setCount((count) => count + 1);
-    setCount(count + 1);
-  };
-  const handleDecrement = () => {
-    //setCount((count) => count - 1);
-    setCount(count - 1);
-  };
-  const handleReset = () => {
-    setCount(0);
+  const [userInfo, setUserInfo] = useState({
+    name: "jack",
+    age: 20,
+    gender: "male",
+  });
+  const handleUpdateUserInfo = () => {
+    setUserInfo((userInfo) => ({
+      ...userInfo,
+      name: "Mike",
+    }));
+    // 또는
+    setUserInfo({
+      name: "Mike",
+      age: 30,
+      gender: "female",
+    });
   };
   return (
     <>
-      <h1>Count: {count}</h1>
-      <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
-      <button onClick={handleReset}>Reset</button>
+      <p>name: {userInfo.name}</p>
+      <p>age: {userInfo.age}</p>
+      <p>gender: {userInfo.gender}</p>
+      <button onClick={handleUpdateUserInfo}>UpdateUserInfo</button>
     </>
   );
 }

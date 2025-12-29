4fc8ce30520475fa411d65e550c1bb6f920956c8
kokk <kokk7310@gmail.com>
2025-12-29 14:47:51 +0900

    (미션) 카운터앱 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,22 @@ import { useState } from "react";
 export default function App() {
   const [count, setCount] = useState(0);
   const handleIncrement = () => {
-    // setCount를 여러번 작성해도 +1씩 증가한다.
-    // (React는 배치 형태로 한꺼번에 모아서 처리하기때문)
-    // setCount(count + 1);
-    // setCount(count + 1);
-    // setCount(count + 1);
-
-    // 함수형을 사용하면 (배치에 영향받지 않고) 항상 최신값 기준으로 업데이트
-    setCount((count) => count + 1); 
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
+    //setCount((count) => count + 1);
+    setCount(count + 1);
+  };
+  const handleDecrement = () => {
+    //setCount((count) => count - 1);
+    setCount(count - 1);
+  };
+  const handleReset = () => {
+    setCount(0);
   };
   return (
     <>
       <h1>Count: {count}</h1>
-      <button onClick={handleIncrement}>increment</button>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 }

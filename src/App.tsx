7508f4a3d156cbaf12a3dc45571d5f3cf4f30c8b
kokk <kokk7310@gmail.com>
2025-12-29 15:55:06 +0900

    상태의 독립성 이해하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
+import FirstCount from "./components/FirstCount";
+import SecondCount from "./components/SecondCount";
 
 export default function App() {
-  const [fruits, setFruits] = useState(["apple", "banana", "orange"]);
-  const handleAddFruit = () => {
-    //setFruits((fruits) => ["melon", ...fruits]);
-    // setFruits((fruits) =>
-    //   fruits.map((fruit) => (fruit === "apple" ? "grape" : fruit))
-    // );
-    //...fruits.slice(0, 1) : 배열 fruits의 0번 인덱스부터 1개를 잘라서
-    setFruits((fruits) => [...fruits.slice(0, 1), "grape", ...fruits.slice(1)]);
-  };
   return (
     <>
-      <p>{fruits.join(", ")}</p>
-      <button onClick={handleAddFruit}>Add Fruit</button>
+      <FirstCount />
+      <SecondCount />
     </>
   );
 }

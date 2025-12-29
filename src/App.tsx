be3895166da1487ee9f21de9eaf202723741736f
kokk <kokk7310@gmail.com>
2025-12-29 15:03:11 +0900

    객체 상태 정의하기 심화

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,45 @@ export default function App() {
     name: "jack",
     age: 20,
     gender: "male",
+    contact: {
+      email: "jack@example.com",
+      phone: "123-456-7890",
+    },
+    address: {
+      home: {
+        street: "123 Main St",
+        city: "New York",
+        zipCode: "10001",
+      },
+      office: {
+        street: "456 Business Ave",
+        city: "New York",
+        zipCode: "10002",
+      },
+    },
   });
+
   const handleUpdateUserInfo = () => {
     setUserInfo((userInfo) => ({
       ...userInfo,
-      name: "Mike",
+      name: "mike",
+      contact: {
+        ...userInfo.contact,
+        email: "sucoding@naver.com",
+      },
+      address: {
+        ...userInfo.address,
+        office: {
+          ...userInfo.address.office,
+          street: "123 Ave",
+        },
+      },
     }));
-    // 또는
-    setUserInfo({
-      name: "Mike",
-      age: 30,
-      gender: "female",
-    });
   };
+
   return (
     <>
-      <p>name: {userInfo.name}</p>
-      <p>age: {userInfo.age}</p>
-      <p>gender: {userInfo.gender}</p>
+      <pre>{JSON.stringify(userInfo, null, 2)}</pre>
       <button onClick={handleUpdateUserInfo}>UpdateUserInfo</button>
     </>
   );

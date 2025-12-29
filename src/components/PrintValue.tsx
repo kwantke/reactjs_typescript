768cf83f6bd5dc961296b8607d6e998925d22097
kokk <kokk7310@gmail.com>
2025-12-29 11:37:21 +0900

    타입 정의하는 방법

--- src/components/PrintValue.tsx
+++ src/components/PrintValue.tsx
@@ -1,27 +1,21 @@
-export default function PrintValue(props: {
-  numberValue: number;
-  stringValue: string;
-  booleanValue: true | false;
-  arrayValue: number[];
-  objectValue: { name: string; age: number };
-  handleClick: () => void;
-}) {
-  const{    
-    numberValue,
-    stringValue,
-    booleanValue,
-    arrayValue,
-    objectValue,
-    handleClick,
-  } = props;
+// interface PrintValueProps {
+//   numberValue: number;
+//   stringValue: string;
+//   booleanValue: boolean;
+//   arrayValue: number[];
+//   objectValue: { name: string; age: number };
+//   handleClick: () => void;
+// }
+
+export default function PrintValue(props: PrintValueProps) {
   return (
     <>
-      <p>number: {numberValue}</p>
-      <p>stringValue: {stringValue}</p>
-      <p>booleanValue: {booleanValue.toString()}</p>
-      <p>arrayValue: {arrayValue}</p>
-      <p>objectValue:{JSON.stringify(objectValue)}</p>
-      <p>function: {handleClick.toString()}</p>
+      <p>number: {props.numberValue}</p>
+      <p>stringValue: {props.stringValue}</p>
+      <p>booleanValue: {props.booleanValue.toString()}</p>
+      <p>arrayValue: {props.arrayValue}</p>
+      <p>objectValue:{JSON.stringify(props.objectValue)}</p>
+      <p>function: {props.handleClick.toString()}</p>
     </>
   );
 }

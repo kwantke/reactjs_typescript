2ba4244d4199ed7179b91183ae3d67fb199f12c3
kokk <kokk7310@gmail.com>
2025-12-29 21:43:26 +0900

    key 속성 고유하게 하기

--- src/components/fruits/FruitList.tsx
+++ src/components/fruits/FruitList.tsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
-
+import { v4 as uuidv4 } from "uuid";
 export default function FruitList() {
-  const [items, setItems] = useState(["사과", "바나나", "오렌지"]);
+  const items = ["사과", "바나나", "오렌지"];
+  const [fruits, setfFruits] = useState(() =>
+    items.map((item) => ({ id: uuidv4(), value: item }))
+  );
   const handleAddItem = () => {
-    setItems((items) => [...items, "포도"]);
+    setfFruits((fruits) => [{ id: uuidv4(), value: "grape" }, ...fruits]);
   };
 
-  const isGrapeAdded = items.includes("포도");
+  const isGrapeAdded = fruits.some((fruit) => fruit.value === "grape");
   return (
     <>
       <h1>FruitList Component</h1>
       <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
+        {fruits.map((fruit) => (
+          <li key={fruit.id}>
+            {/* {fruit.value} */}
+            <input type="text" placeholder={fruit.value} />
+          </li>
         ))}
       </ul>
       <button onClick={handleAddItem} disabled={isGrapeAdded}>

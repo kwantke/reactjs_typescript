54b0e14eb644997132b7a57e484cdf93cf725660
kokk <kokk7310@gmail.com>
2025-12-30 16:07:56 +0900

    Redux slice 한개 더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
 import Count from "./components/count2/Count";
 import CountOutside from "./components/count2/CountOutside";
+import Theme from "./components/theme/Theme";
+import ThemeButton from "./components/theme/ThemeButton";
 
 export default function App() {
   return (
     <>
       <Count />
       <CountOutside />
+      <Theme />
+      <ThemeButton />
     </>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./features/counter/counterSlice";
+import themeSlice from "./features/theme/themeSlice";
+
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
+    theme: themeSlice,
   },
 });
 

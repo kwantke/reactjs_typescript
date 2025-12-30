60530c186db3112f000610a613e10a7ff893ec50
kokk <kokk7310@gmail.com>
2025-12-30 17:13:11 +0900

    tailwind css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import Count from "./components/count2/Count";
-import CountOutside from "./components/count2/CountOutside";
-import Theme from "./components/theme/Theme";
-import ThemeButton from "./components/theme/ThemeButton";
-
 export default function App() {
   return (
     <>
-      <Count />
-      <CountOutside />
-      <Theme />
-      <ThemeButton />
+      <h1>App Component</h1>
     </>
   );
 }

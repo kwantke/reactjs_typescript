b9d0304339e9f0f9ad38685fcf3d1fff38516f6d
kokk <kokk7310@gmail.com>
2025-12-30 13:04:24 +0900

    커스텀 훅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
-import Input from "./components/uncontrolled/Input";
+import useInput from "./hooks/useInput";
 
 export default function App() {
+  const { value: email, handleValueChange: handleEmailChange } = useInput("");
+  const { value: password, handleValueChange: handlePasswordChange } =
+    useInput("");
+  const { value: name, handleValueChange: handleNameChange } = useInput("");
+
+  // const [password, setPassword] = useState("");
+  // const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setPassword(e.target.value);
+  // };
+
+  // const [name, setName] = useState("");
+  // const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setName(e.target.value);
+  // };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log({ email, password, name });
+  };
+
   return (
     <>
-      <Input />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={handleNameChange}
+        />
+        <button type="submit">제출</button>
+      </form>
     </>
   );
 }

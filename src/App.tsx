e0fa551461e4fe8f215722099d30f8805d97966b
kokk <kokk7310@gmail.com>
2025-12-30 13:33:07 +0900

    vaidation 커스텀

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import useInput from "./hooks/useInput";
+import {
+  validateEmail,
+  validateName,
+  validatePassword,
+} from "./util/validation";
 
 export default function App() {
-  const { value: email, handleValueChange: handleEmailChange } = useInput("");
-  const { value: password, handleValueChange: handlePasswordChange } =
-    useInput("");
-  const { value: name, handleValueChange: handleNameChange } = useInput("");
-
-  // const [password, setPassword] = useState("");
-  // const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setPassword(e.target.value);
-  // };
-
-  // const [name, setName] = useState("");
-  // const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setName(e.target.value);
-  // };
+  const {
+    value: email,
+    handleValueChange: handleEmailChange,
+    error: emailError,
+  } = useInput("", validateEmail);
+  const {
+    value: password,
+    handleValueChange: handlePasswordChange,
+    error: passwordError,
+  } = useInput("", validatePassword);
+  const {
+    value: name,
+    handleValueChange: handleNameChange,
+    error: nameError,
+  } = useInput("", validateName);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log({ email, password, name });
+    if (emailError) {
+      alert(emailError);
+      return;
+    }
+
+    if (passwordError) {
+      alert(passwordError);
+      return;
+    }
+
+    if (nameError) {
+      alert(nameError);
+      return;
+    }
   };
 
   return (
@@ -29,19 +49,25 @@ export default function App() {
           placeholder="email"
           value={email}
           onChange={handleEmailChange}
+          required
         />
+        {emailError && <p>{emailError}</p>}
         <input
           type="password"
           placeholder="password"
           value={password}
           onChange={handlePasswordChange}
+          required
         />
+        {passwordError && <p>{passwordError}</p>}
         <input
           type="text"
           placeholder="name"
           value={name}
           onChange={handleNameChange}
+          required
         />
+        {nameError && <p>{nameError}</p>}
         <button type="submit">제출</button>
       </form>
     </>

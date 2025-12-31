e030f33f180855dadf86ca408e70ae2ecc258f6b
kokk <kokk7310@gmail.com>
2025-12-31 17:22:36 +0900

    zustand immer 사용법

--- src/components/countZustand/store/countStore.ts
+++ src/components/countZustand/store/countStore.ts
@@ -4,6 +4,7 @@ import {
   persist,
   subscribeWithSelector,
 } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
 type CountStore = {
   count: number;
@@ -15,18 +16,23 @@ type CountStore = {
 export const useCountStore = create<CountStore>()(
   subscribeWithSelector(
     persist(
-      (set) => ({
+      immer((set) => ({
         count: 0,
-        decrement: () => set((state) => ({ count: state.count - 1 })),
+        decrement: () =>
+          set((state) => {
+            state.count -= 1;
+          }),
         increment: (amount: number, amount2: number) =>
-          set((state) => ({ count: state.count + amount + amount2 })),
+          set((state) => {
+            state.count += amount + amount2;
+          }),
         // 비동기로 구현 방법
         // increment: async (amount: number, amount2: number) => {
         //   await new Promise((resolve) => setTimeout(resolve, 1000));
         //   set((state) => ({ count: state.count + amount + amount2 }));
         // },
         reset: () => set({ count: 0 }),
-      }),
+      })),
       {
         name: "count-storage",
         // localStorage가 아닌 세션 스토리지에 저장하고 싶을경우

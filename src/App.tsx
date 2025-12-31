2673a4fae6f4b081720d649dad1fbde8c787e09f
kokk <kokk7310@gmail.com>
2025-12-31 13:51:06 +0900

    useMemo 사용법

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
-import ChildA from "./components/child/ChildA";
+import { useMemo, useState } from "react";
+
+const initialItems = new Array(29_999_999).fill(0).map((_, i) => {
+  return {
+    id: i,
+    selected: i === 29_999_998,
+  };
+});
 
 export default function App() {
   const [count, setCount] = useState(0);
-  console.log("App");
+
+  // 불필요한 연산을 만들어준 것
+  // 3000만개의 배열 데이터를 렌더링 마다 재생성하고 있음
+  const selectItems = useMemo(
+    () => initialItems.find((item) => item.selected),
+    []
+  );
   return (
     <>
-      <h1>count: {count}</h1>
-      <button onClick={() => setCount((count) => count + 1)}>증가</button>
-      <ChildA count={count} />
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        증가
+      </button>
+      <p>{selectItems?.id}</p>
     </>
   );
 }
--- src/components/child/ChildA.tsx
+++ src/components/child/ChildA.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ChildB from "./ChildB";
 
-export default React.memo(function ChildA({ count }: { count: number }) {
+export default React.memo(function ChildA({
+  increment,
+}: {
+  increment: () => void;
+}) {
   console.log("ChildA");
   // const [count, setCount] = useState(0);
   return (
     <>
-      <h1>ChildA Component: {count}</h1>
-      {/* <button onClick={() => setCount((count) => count + 1)}>증가</button> */}
+      <h1>ChildA Component</h1>
+      <button onClick={increment}>증가</button>
       <ChildB />
     </>
   );

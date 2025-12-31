b482b26028dad78b2d3bac89174bf45db991c800
kokk <kokk7310@gmail.com>
2025-12-31 14:34:23 +0900

    ErrorBoundary 사용법 - 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { lazy, Suspense, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 // import ChildA from "./components/child/ChildA";
 // import ChildB from "./components/child/ChildB";
 // const ChildA = React.lazy(() => import("./components/child/ChildA"));
@@ -12,12 +13,18 @@ export default function App() {
       <button onClick={() => setIsShow((isShow) => !isShow)}>토글</button>
       {isShow && (
         <>
-          <Suspense fallback={<h1>ChildA Loading..</h1>}>
-            <ChildA />
-          </Suspense>
-          <Suspense fallback={<h1>ChildB Loading..</h1>}>
-            <ChildB />
-          </Suspense>
+          {/*ErrorBoundary: 오류 발생시 대체 ui 화면를 표시함  */}
+          <ErrorBoundary fallback={<p>ChildB went wrong</p>}>
+            {/*Suspense: 화면 지연시 대체 ui 화면을 임시로 표시함 */}
+            <Suspense fallback={<h1>ChildA Loading..</h1>}>
+              <ChildA />
+            </Suspense>
+          </ErrorBoundary>
+          <ErrorBoundary fallback={<p>ChildB went wrong</p>}>
+            <Suspense fallback={<h1>ChildB Loading..</h1>}>
+              <ChildB />
+            </Suspense>
+          </ErrorBoundary>
         </>
       )}
     </>
--- src/components/child/ChildB.tsx
+++ src/components/child/ChildB.tsx
@@ -2,6 +2,10 @@ import ChildC from "./ChildC";
 
 export default function ChildB() {
   console.log("ChildB");
+  const randomNum = Math.floor(Math.random() * 2) + 1;
+  if (randomNum === 1) {
+    //throw new Error("random number is 1");
+  }
   return (
     <>
       <h1>ChildB Component</h1>

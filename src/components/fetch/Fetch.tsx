bf0eb1b7a2f17dd04c3cab24985fd429126dc7e9
kokk <kokk7310@gmail.com>
2025-12-31 19:50:34 +0900

    fetch - signal 설정

--- src/components/fetch/Fetch.tsx
+++ src/components/fetch/Fetch.tsx
@@ -7,12 +7,15 @@ interface Posts {
 }
 export default function Fetch() {
   const [posts, setPosts] = useState<Posts[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
   useEffect(() => {
-    setIsLoading(true);
-    setError("");
-    fetch("http://localhost:3000/postsf")
+    const controller = new AbortController();
+    //setIsLoading(true);
+    //setError("");
+    fetch("http://localhost:3000/posts", {
+      signal: controller.signal,
+    })
       .then((resposne) => {
         console.log(resposne);
         if (!resposne.ok) throw new Error("네트워크 통신 오류");
@@ -23,11 +26,13 @@ export default function Fetch() {
       })
       .catch((e) => {
         console.log("오류: " + e);
-        setError(e instanceof Error ? e.message : "unknown error");
+        if (e instanceof Error && e.name !== "AbortError") setError(e.message);
+        //setError(e instanceof Error ? e.message : "unknown error");
       })
       .finally(() => {
-        setIsLoading(false);
+        if (!controller.signal.aborted) setIsLoading(false);
       });
+    return () => controller.abort();
   }, []);
 
   if (error) return <p>Error: {error}</p>;

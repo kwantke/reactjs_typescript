2e33b22bfecfaeb0a8fcb1f1e363ceb84afe9d6f
kokk <kokk7310@gmail.com>
2025-12-31 19:29:31 +0900

    fetch error 설정

--- src/components/fetch/Fetch.tsx
+++ src/components/fetch/Fetch.tsx
@@ -8,21 +8,29 @@ interface Posts {
 export default function Fetch() {
   const [posts, setPosts] = useState<Posts[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
   useEffect(() => {
     setIsLoading(true);
-    fetch("http://localhost:3000/posts")
+    setError("");
+    fetch("http://localhost:3000/postsf")
       .then((resposne) => {
         console.log(resposne);
+        if (!resposne.ok) throw new Error("네트워크 통신 오류");
         return resposne.json();
       })
       .then((data) => {
         setPosts(data);
+      })
+      .catch((e) => {
+        console.log("오류: " + e);
+        setError(e instanceof Error ? e.message : "unknown error");
       })
       .finally(() => {
         setIsLoading(false);
       });
   }, []);
 
+  if (error) return <p>Error: {error}</p>;
   return (
     <>
       <h3>Fetch</h3>

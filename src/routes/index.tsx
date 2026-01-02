acc30ebc6a98a5527de4572027e5c03a17c1e434
kokk <kokk7310@gmail.com>
2026-01-02 21:10:42 +0900

    라우트 프리픽스

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,32 +4,38 @@ import About from "./pages/About";
 import DashboardHome from "./pages/dashboard/DashboardHome";
 import Dashboard from "./pages/dashboard/Dashboard";
 import DashboardSetting from "./pages/dashboard/DashboardSetting";
+import Default from "./pages/layouts/Default";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    Component: About,
-  },
-  {
-    path: "/dashboard",
-    Component: Dashboard,
+    Component: Default,
     children: [
       {
-        // path: "",
-        index: true, // 인덱스 라우트
-        Component: DashboardHome,
+        path: "/",
+        element: <Home />,
       },
       {
-        path: "setting", // dashboard/setting 을 적용하려면 <Outlet/> 설정 필요
-        Component: DashboardSetting,
+        path: "/about",
+        Component: About,
+      },
+      {
+        path: "/dashboard",
+        //Component: Dashboard,
         children: [
           {
-            path: "custom", // dashboard/setting/custom
-            element: <h1>DashbottardSetting Hoeeme</h1>,
+            // path: "",
+            index: true, // 인덱스 라우트
+            Component: DashboardHome,
+          },
+          {
+            path: "setting", // dashboard/setting 을 적용하려면 <Outlet/> 설정 필요
+            Component: DashboardSetting,
+            children: [
+              {
+                path: "custom", // dashboard/setting/custom
+                element: <h1>DashbottardSetting Hoeeme</h1>,
+              },
+            ],
           },
         ],
       },
